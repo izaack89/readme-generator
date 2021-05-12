70d5d7ed6ec199c87213b6121e509ed0cb96be81
German Ramirez <mitsuominagi@gmail.com>
2021-05-12 17:52:06 -0500

    Added comments to the code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  // Here I check if the license is none if not I check what License to set a color for the badge 
   if (license !== "None") {        
     let colorBadge = '';
     switch (license) {
@@ -41,11 +42,13 @@ function renderLicenseBadge(license) {
         colorBadge = 'orange';
         break;
     }
+    // I create the License link of the Badge 
     const licenseLink = `https://img.shields.io/badge/licenses-${license.replace(/\s/g, "_")}-${colorBadge}.svg`;
     return `
 ![GitHub license](${licenseLink})
     `;
   } else {
+    // if the License is NONE I return blank
     return '';
   }
 }
@@ -53,6 +56,7 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  // Here I check if the license is none if not I check what License to set the link of the License information
   if (license !== "None") {
     let link = '';
     switch (license) {
@@ -86,6 +90,7 @@ function renderLicenseLink(license) {
     }
     return `[${license}](${link})`
   } else {
+    // if the License is NONE I return blank
     return '';
   }
 }
@@ -109,9 +114,9 @@ ${badgeLink}`;
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = function (data) {
-
+  // I get the License Section 
   const licenseSection = renderLicenseSection(data.projectLicense);
-  
+  // Created a variable that will be the table content , this will check if the user selected a License so it can be aaded or if not it will not add the License section 
   let tableContent = `
 - [Installation](#installation)
 - [Usage](#usage)
@@ -127,7 +132,7 @@ const generateMarkdown = function (data) {
 - [Tests](#tests)
 - [Questions](#questions)`;
   }
-  
+  // I created the constant variable for the template of the readme and I append the information that the user provided using inquirer
   const templateReadme = `
 # ${data.projectTitle}
 
@@ -175,8 +180,8 @@ ${data.projectTest}
 If you have any questions about the repository, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.githubName}](https://github.com/${data.githubName})
 
   `;
+  // I return the template already completed with the user information
   return templateReadme;
 }
-
-
+// Here the function of generateMarkdown is exported in order to be used on the file by using require property 
 module.exports = generateMarkdown
