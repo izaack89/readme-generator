6128f9ca0295342f7f5cedf0c86f7019377ae8f4
German Ramirez <mitsuominagi@gmail.com>
2021-05-12 17:19:49 -0500

    Added the code and comments on this file , also fixed some misspelling words on the question section

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
+//I create the question according to the structure that inquirer need
 const questions = [
     {
         name: 'projectTitle',
@@ -10,7 +11,7 @@ const questions = [
     },
     {
         name: 'projectDescription',
-        message: 'Please write a short description of your project'
+        message: 'Please write a short description of your project:'
     },
     {
         name: 'projectInstall',
@@ -24,17 +25,17 @@ const questions = [
     },
     {
         name: 'projectUsage',
-        message: 'What does the user need to know about using the repository'
+        message: 'What does the user need to know about using the repository:'
     },
     {
         name: 'projectGuidelines',
-        message: 'What does the user need to know about contibuting to the repository'
+        message: 'What does the user need to know about contributing to the repository:'
     },
     {
         type: 'list',
         name: 'projectLicense',
         message: 'What kind of license should your project have?',
-        choices: ['Apache 2.0', 'GNU v3.0', 'MIT', 'BSD 2', 'BSD 3', 'Boost SW 1.0', , 'Creative Commons Zero 1.0', 'Eclipse', 'GNU v3.0', 'GNU v2.0', 'GNU 2.1', 'Mozilla', 'The Unlicense', 'None'],
+        choices: ['Apache 2.0', 'GNU v3.0', 'MIT', 'BSD 2', 'BSD 3', 'Boost SW 1.0', 'Creative Commons Zero 1.0', 'Eclipse',  'Mozilla', 'None'],
         default: 'None'
     },
     {
@@ -50,21 +51,24 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-        err ? console.log(err) : console.log('SuccessFully wrote to index.html!')
+    // I get the README content according to the user information provided with inquirer
+    const dataReadme = generateMarkdown(data);
+    // I use File System to create the README File
+    fs.writeFile(fileName, dataReadme, (err) =>
+        err ? console.log(err) : console.log('SuccessFully created your README file!')
     );
  }
 
 // TODO: Create a function to initialize app
 function init() {
-    
+    // I call inquirer, I set on the prompt the question that was defined before 
 inquirer
     .prompt(questions)
-    .then(answers => {
-        console.info('Answers:', answers);
+    .then(data => {
+        // Once I display the question and get the answers I call the function that will write the README
+        // I decied that the file will be exported to a folder this is in order to avoid to delete the README of this project
+        writeToFile("./exported/README.md", data);
     });
-
 }
-
 // Function call to initialize app
 init();
