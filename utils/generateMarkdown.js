82b70ef19a56af2c8e5041a05a10b8ce1666a55b
German Ramirez <mitsuominagi@gmail.com>
2021-05-12 18:27:23 -0500

    Deleted a console log that I used for the testing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -98,7 +98,6 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  console.log('renderLicenseSection');
   if (license !== "None") {
     const licenseLink = renderLicenseLink(license);
     const badgeLink = renderLicenseBadge(license);
